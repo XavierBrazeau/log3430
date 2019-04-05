2239b39fc8d28e5ae7c70234e6c3b523c8601d78
Xavier Brazeau <brazeaux@hotmail.ca>
2019-04-04 21:21:55 -0400

    changementt de bitpartiteXExtended

--- TP3/algs4-master/src/main/java/tests/testBipartiteXExtended.java
+++ TP3/algs4-master/src/main/java/tests/testBipartiteXExtended.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Iterator;
+import java.util.List;
 
 import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.BipartiteX;
@@ -16,14 +17,19 @@ import org.junit.jupiter.api.Test;
 class testBipartiteXExtended {
 	@Test
 	void testNonBipartite() {
-		Graph graph = new Graph(3);
+		Graph graph = new Graph(4);
 		
 		graph.addEdge(0, 2);
 		graph.addEdge(1, 2);
 		graph.addEdge(0, 1);
 		
 		BipartiteXExtended bipartite = new BipartiteXExtended(graph);
-		assertNotNull(bipartite.getVerticesWithAnEdgeToB(2));
+		List<Integer> test = bipartite.getVerticesWithAnEdgeToB(2);
+		assertTrue(test.get(0).equals(0));
+		assertTrue(test.get(1).equals(1));
+		
+		List<Integer> test2 = bipartite.getVerticesWithAnEdgeToB(4);
+		
 		assertFalse(bipartite.isBipartite());
 		
 		assertNotNull(bipartite.oddCycle());
